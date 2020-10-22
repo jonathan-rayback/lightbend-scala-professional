b86500c65d61e80852c4315efddbc65f061d36b8
Jonathan Rayback <jonathan.rayback@evernym.com>
2020-10-22 09:56:55 -0600

    adjust greeting message

--- sbt-by-example/src/main/scala/example/Hello.scala
+++ sbt-by-example/src/main/scala/example/Hello.scala
@@ -5,6 +5,6 @@ import core.Weather
 
 object Hello extends App {
   val w = Await.result(Weather.weather, 10.seconds)
-  println(s"Hello! The weather in New York is $w.")
+  println(s"Hello Big Apple! The weather in New York is $w.")
   Weather.http.close()
 }
