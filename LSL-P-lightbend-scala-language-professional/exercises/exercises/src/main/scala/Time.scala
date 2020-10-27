b21d8307635f7eef30c23c66e00dd5246870d98d
Jonathan Rayback <jonathan.rayback@evernym.com>
2020-10-27 17:34:23 -0600

    add default values to time class

--- LSL-P-lightbend-scala-language-professional/exercises/exercises/src/main/scala/Time.scala
+++ LSL-P-lightbend-scala-language-professional/exercises/exercises/src/main/scala/Time.scala
@@ -1,4 +1,4 @@
-class Time (val hours : Int, val minutes : Int) {
+class Time (val hours : Int = 0, val minutes : Int = 0) {
   // TODO - Verify hours is within 0 and 23
   // TODO - Verify minutes is whithin 0 and 59
   val asMinutes : Int = (hours * 60) + minutes
