2c62e99b5f0289ae38c648cebb7712c29a13ff01
Jonathan Rayback <jonathan.rayback@evernym.com>
2020-11-20 17:53:04 -0700

    update files with TrainInfo class

--- LSL-P-lightbend-scala-language-professional/exercises/exercises/src/main/scala/com/lightbend/training/scalatrain/Train.scala
+++ LSL-P-lightbend-scala-language-professional/exercises/exercises/src/main/scala/com/lightbend/training/scalatrain/Train.scala
@@ -2,7 +2,15 @@ package com.lightbend.training.scalatrain
 
 import scala.collection.immutable.Seq
 
-case class Train(kind: String, number: Int, schedule: Seq[(Time, Station)]) {
+sealed abstract class TrainInfo {
+  def number: Int
+}
+
+case class InterCityExpress(number: Int, hasWifi: Boolean = false) extends TrainInfo
+case class RegionalExpress(number: Int) extends TrainInfo
+case class BavarianRegional(number: Int) extends TrainInfo
+
+case class Train(info: TrainInfo, schedule: Seq[(Time, Station)]) {
   require(schedule.length >= 2, "Must have at least two stations in the schedule for this train.")
   val stations: Seq[Station] = schedule.map(_._2)
 
